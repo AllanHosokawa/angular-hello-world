21c91d0ad880bf49380fca03d8247576432a5967
AllanHosokawa <7543284+AllanHosokawa@users.noreply.github.com>
2019-02-24 14:48:38 -0300

    First commit

---
+++ src/app/github-profile/github-profile.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-github-profile',
+  templateUrl: './github-profile.component.html',
+  styleUrls: ['./github-profile.component.css']
+})
+export class GithubProfileComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(id);
+
+    // this.activatedRoute.paramMap
+    //   .subscribe(params => {
+    //     let id = +params.get('id');
+    //     console.log(id);
+    //   });
+  }
+
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    });
+  }
+}
